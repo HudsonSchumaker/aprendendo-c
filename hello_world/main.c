030437501136da85e3917f04e0a00cdd03ed953f
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-05-27 22:58:51 +0200

    Create main.c

---
+++ hello_world/main.c
@@ -0,0 +1,13 @@
+/*
+  aprendendo-c
+  Olá mundo (Hello world)
+*/
+
+// inclui no projeto a biblioteca padrao de I/O 
+#include <stdio.h>
+
+// metódo main
+int main(int argc, const char * argv[]) {
+    printf("Olá mundo !\n");
+    return 0;
+}
