e6b2fbbd84eba45959d230837d846b3509201c6c
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-05-27 23:03:05 +0200

    Create main.c

---
+++ comments/main.c
@@ -0,0 +1,18 @@
+/*
+  aprendendo-c
+  Comentários (Comments)
+*/
+
+// inclui no projeto a biblioteca padrao de I/O 
+#include <stdio.h>
+
+/*
+    bloco de comentarios
+    metodo principal
+*/
+int main(int argc, const char * argv[]) {
+
+    // essa linha imprimi no console
+    printf("Olá mundo !\n");
+    return 0;
+}
