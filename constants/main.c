893f85fb3cdc51e2295fab3dd59cc9f6ac67c63a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-05-28 20:19:44 +0200

    Create main.c

---
+++ constants/main.c
@@ -0,0 +1,23 @@
+/*
+  aprendendo-c
+  constantes - constants
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char * argv[]) {
+    
+    /*
+        constante (const) = valor fixo em uma varivel, nao pode ser mudado
+        dutante execucao do programa.
+     */
+    
+    const float pi = 3.141592;
+    
+    // pi = 1.0f; // nao funciona
+    
+    printf("%f", pi);
+    
+    return 0;
+}
