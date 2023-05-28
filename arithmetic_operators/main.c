93a47bdc2e1cdcd42b7f5184986227ca4abab2ce
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-05-28 20:28:00 +0200

    Create main.c

---
+++ arithmetic_operators/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    /*
+        + adicao
+        - subtracao
+        * multiplicacao
+        / divisao
+        % modulo
+        ++ incremento
+        -- decremento
+    */
+    
+    int x = 6;
+    int y = 2;
+    int z = 0;
+    
+    z = x + y;
+    printf("%d\n", z);
+    
+    z = x - y;
+    printf("%d\n", z);
+    
+    z = x * y;
+    printf("%d\n", z);
+    
+    z = x / y;
+    printf("%d\n", z);
+    
+    z = x % y;
+    printf("%d\n", z);
+    
+    z++;
+    printf("%d\n", z);
+    
+    z--;
+    printf("%d\n", z);
+    
+    return 0;
+}
