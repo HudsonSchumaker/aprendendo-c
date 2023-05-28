e8623f2aca5ed2abe271e273c3355e2c20b1a60c
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-05-28 20:08:06 +0200

    Update main.c

--- data_types/main.c
+++ data_types/main.c
@@ -1,1 +1,30 @@
+/*
+ aprendendo-c
+ tipo de dados - data type
+*/
 
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char * argv[]) {
+
+    // https://www.geeksforgeeks.org/data-types-in-c/
+
+    char c = 'a';       // caracter simples %c
+    char a[] = "Mario"; // array de caracteres %s
+
+    int i = 10; // numero inteiro %d
+
+    float f = 3.141592;  // 4 bytes 6-7 digitos de precisao %f
+    double d = 3.14159265358979323846; // 8 bytes 15-16 digitos de precisao %lf
+
+    bool b = true; // 1 byte (true ou false) %d
+
+    printf("%c\n", c);
+    printf("%s\n", a);
+    printf("%d\n", i);
+    printf("%f\n", f);
+    printf("%lf\n", d);
+
+    return 0;
+}
