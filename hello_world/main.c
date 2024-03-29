b7862e2535b675dcbd40bf0223123be56d9485e1
HudsonSchumaker <hudson.schumaker@me.com>
2024-03-29 10:16:36 +0100

    update hello world

--- hello_world/main.c
+++ hello_world/main.c
@@ -11,3 +11,11 @@ int main(int argc, const char * argv[]) {
     printf("Olá mundo !\n");
     return 0;
 }
+
+/*
+  main é a função principal onde começa a execução de qualquer programa C.
+  Os parâmetros argc e argv[] são argumentos de linha de comando.
+  argc é a contagem do total de argumentos de linha de comando passados para o executável na 
+  execução (incluindo o nome do executável como primeiro argumento). 
+  argv[] é uma matriz de ponteiros de caracteres listando todos os argumentos.
+*/
