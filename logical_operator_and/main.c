90e9d6edf06b9de7d2bf61ddc4cd37c14bfdef07
Hudson Schumaker <hudson.schumaker@me.com>
2025-03-17 15:20:05 +0100

    add AND operator.

---
+++ logical_operator_and/main.c
@@ -0,0 +1,54 @@
+/*
+  aprendendo-c
+  Condicional Se (If staments)
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char *argv[])
+{
+    /*
+    Logical Operators
+    Operadores lógicos são usados para combinar duas ou mais condições.
+    O operador lógico AND (&&) é verdadeiro se todas as condições forem verdadeiras.
+    Tabela verdade do operador lógico AND:
+    +-------+-------+-----------+
+    |   A   |   B   | A && B    |
+    +-------+-------+-----------+
+    |   V   |   V   |     V     |
+    |   V   |   F   |     F     |
+    |   F   |   V   |     F     |
+    |   F   |   F   |     F     |
+    +-------+-------+-----------+
+    */
+
+    int num = 10;
+
+    // Instrução if com operador lógico AND
+    if (num > 5 && num < 20)
+    {
+        printf("Número é maior que 5 e menor que 20\n");
+    }
+
+    bool a = true;
+    bool b = false;
+    bool c = true;
+
+    // Instrução if com operador lógico AND
+    if (a && c)
+    {
+        printf("a e c são verdadeiros\n");
+    }
+
+    // Instrução if com operador lógico AND
+    if (a && b)
+    {
+        // Não será executado
+        printf("a e b são verdadeiros\n");
+    } else {
+        printf("a e b nao são verdadeiros\n");
+    }
+
+    return 0;
+}
