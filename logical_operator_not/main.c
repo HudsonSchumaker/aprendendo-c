be34c66d2ccd08c8b1ebf35efab541392580637a
Hudson Schumaker <hudson.schumaker@me.com>
2025-03-29 14:42:56 +0100

    add not operator

---
+++ logical_operator_not/main.c
@@ -0,0 +1,39 @@
+/*
+  aprendendo-c
+  Operador ! || (Operator NOT)
+*/
+
+// inclui no projeto a biblioteca padrao de I/O (input, output) entrada e saída
+#include <stdio.h>
+// inclui no projeto a biblioteca padrao de booleanos (true, false)
+#include <stdbool.h>
+
+int main(int argc, const char *argv[])
+{
+    /*
+    Logical Operators
+    Operadores lógicos são usados para combinar duas ou mais condições.
+    O operador lógico NOT (!) inverte o valor de uma condição.
+    Tabela verdade do operador lógico NOT:
+    +-------+-----------+ 
+    |   A   |    !A     |
+    +-------+-----------+
+    |   V   |     F     |
+    |   F   |     V     |
+    +-------+-----------+
+    */
+
+    int num = 10;
+
+    // Instrução if com operador lógico NOT
+    if (!(num > 5))
+    {
+        printf("Número não é maior que 5\n");
+    }
+    else
+    {
+        printf("Número é maior que 5\n");
+    }
+
+    return 0;
+}
